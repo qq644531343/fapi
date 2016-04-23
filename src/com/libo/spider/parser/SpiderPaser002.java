5a483bc5242a673b61749921f2301221d02f4418
baihe.llb <baihe.llb@alibaba-inc.com>
2016-04-24 01:06:41 +0800

    初步构建三层爬虫策略

--- src/com/libo/spider/parser/SpiderPaser002.java
+++ src/com/libo/spider/parser/SpiderPaser002.java
@@ -1,7 +1,11 @@
 package com.libo.spider.parser;
 
 import java.util.ArrayList;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 import org.htmlparser.tags.Div;
 import org.htmlparser.tags.HeadingTag;
@@ -23,15 +27,24 @@ import com.libo.tools.XLog;
  */
 
 public class SpiderPaser002 implements SpiderPaserInterface {
+	
+	private BlockingQueue<Runnable> queue = new LinkedBlockingQueue<Runnable>(); 
+	private ThreadPoolExecutor executor = new ThreadPoolExecutor(5, 2000, 100000, TimeUnit.HOURS, queue); 
 
 	@Override
 	public void parser(String htmlString, HTMLContentModel info) throws Exception {
 		
 		if (info.getUserinfo() == null) {
 			parseForOrigin(htmlString, info);
+			System.out.println("xxxxx" + executor.getTaskCount() + "  " + executor.getCompletedTaskCount());
+			
+			executor.shutdown();
 		}else if("second".equals(info.getUserinfo())) {
 			parseForSecond(htmlString, info);
+		}else if("third".equals(info.getUserinfo())) {
+			parseForThrid(htmlString, info);
 		}
+
 	}
 	
 	//顶层解析
@@ -41,7 +54,7 @@ public class SpiderPaser002 implements SpiderPaserInterface {
 		if (divList.size() > 0) {
 			
 		   CountDownLatch latch=new CountDownLatch(divList.size());
-		   ArrayList<Thread> tlist = new ArrayList<Thread>();
+		   ArrayList<Runnable> tlist = new ArrayList<Runnable>();
 			
 			NodeIterator it = divList.elements();
 			while (it.hasMoreNodes()) {
@@ -55,11 +68,11 @@ public class SpiderPaser002 implements SpiderPaserInterface {
 				System.out.println(linkUrl);
 				
 				//开始二级解析
-				Worker work = new Worker(latch, linkUrl);
+				Worker work = new Worker(latch, linkUrl, "second");
 				tlist.add(work);
 			}
-			for (Thread thread : tlist) {
-				thread.start();
+			for (Runnable work : tlist) {
+				new Thread(work).start();
 			}
 			latch.await();
 			XLog.logger.info("二级任务执行完毕");
@@ -83,6 +96,9 @@ public class SpiderPaser002 implements SpiderPaserInterface {
 					String name = link.getLinkText();
 					String url = "http://www.100ppi.com/price/" + link.extractLink();
 					System.out.println(name + " " + url);
+					
+					executor.execute(new Worker(url, "third"));
+					System.out.println("执行三级任务：" + url);
 				}
 			}
 			
@@ -92,12 +108,23 @@ public class SpiderPaser002 implements SpiderPaserInterface {
 		
 	}
 	
-	class Worker extends Thread {
+	private void parseForThrid(String htmlString, HTMLContentModel info) {
+		System.out.println("三级解析:" + info.getOriginUrl());
+	}
+	
+	class Worker implements Runnable {
 		CountDownLatch latch; 
 		String		   linkUrl;
+		String		   userinfo;
 		
-		public Worker(CountDownLatch latch, String linkUrl) {
+		public Worker(CountDownLatch latch, String linkUrl, String userinfo) {
 			this.latch = latch;
+			this.linkUrl = linkUrl;
+			this.userinfo = userinfo;
+		}
+		
+		public Worker(String linkUrl , String userinfo) {
+			this.userinfo = userinfo;
 			this.linkUrl = linkUrl;
 		}
 		
@@ -109,11 +136,13 @@ public class SpiderPaser002 implements SpiderPaserInterface {
 			info.setTid("002");
 			
 			HTMLContentModel model = HTMLGetter.getHTMLContentFromInfo(info);
-			model.setUserinfo("second");
+			model.setUserinfo(userinfo);
 			HTMLParser.parser(model);
 			
-			latch.countDown();
-			super.run();
+			if (latch != null) {
+				latch.countDown();
+			}
+	
 		}
 	}
 
